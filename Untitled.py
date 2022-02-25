d8d80dfa902068122aff31192c8a2d208b2a9f6d
MlleLauraka <77595617+MlleLauraka@users.noreply.github.com>
2022-02-25 14:14:21 -0500

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -24,6 +24,7 @@ import google.cloud
 import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
+bigquery_client = bigquery.Client()
 
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
