5daf54aad9e28d84d6771e494361b6b694d96a66
MlleLauraka <77595617+MlleLauraka@users.noreply.github.com>
2022-03-01 22:08:08 -0500

    Update crypto_categories_streamlit.py

--- crypto_categories_streamlit.py
+++ crypto_categories_streamlit.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
 
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
-import streamlit as st 
-
-
-# In[4]:
-
-
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Crypto Categories Dashboard", page_icon=":bar_chart:", layout="wide")
 
-# ---- READ EXCEL ----
 
-df = pd.read_excel("/Users/Laura/Desktop/PYTHON/Projects/Crypto_Categories/Crypto_Categories.xlsx", engine="openpyxl", sheet_name="Sheet1")
-df.head()
+# ---- Get an Excel File with Streamlit ----
+@st.cache
+def get_data_from_excel():
+    df = pd.read_excel(
+        io="/Users/Laura/Desktop/PYTHON/Projects/Crypto_Categories/Crypto_Categories.xlsx",
+        engine="openpyxl",
+        sheet_name="Sheet1",
+        skiprows=0,
+        usecols="B:R",
 
-# ---- SIDEBAR ----
+    )
+
+# add nrows=142, if issues in the future to retrieve data
+
+    return df
+df = get_data_from_excel()
+
+# ---- Menu Bar (Side Bar) ----
 st.sidebar.header("Please Filter Here:")
 Name = st.sidebar.multiselect(
     "Select the Category:",
@@ -30,20 +31,18 @@ Name = st.sidebar.multiselect(
     default=df["name"].unique()
 )
 
-
 df_selection = df.query(
-    "Name == @name"
+    "name == @Name"
 )
 
-# ---- MAINPAGE ----
+# ---- Main Page ----
 st.title(":bar_chart: Crypto Categories Dashboard")
 st.markdown("##")
 
-total_sales = int(df_selection["name"].count())
-total_sales 
+
 # TOP KPI's
 number_categories = int(df_selection["name"].count())
-average_rating_mc = round(df_selection["market_cap_change_24h"].mean(), 1)
+average_rating_mc = round(df_selection["market_cap_change_24h"].mean(),1)
 average_rating_vm = round(df_selection["volume_24h"].mean(), 2)
 
 left_column, middle_column, right_column = st.columns(3)
@@ -56,50 +55,79 @@ with middle_column:
 with right_column:
     st.subheader("Average Market Volume:")
     st.subheader(f"US $ {average_rating_vm}")
-
 st.markdown("""---""")
 
-# SALES BY PRODUCT LINE [BAR CHART]
-market_cap_chart = (
-    df_selection.groupby(by=["name"])[["market_cap"]].sort_values(by="market_cap").sum()
-)
-fig_product_sales = px.bar(
-    market_cap_chart,
-    x="market_cap",
-    y=market_cap_chart.index,
-    orientation="h",
-    title="<b>Market Capitalization by Category </b>",
-    color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
-    template="plotly_white",
-)
-fig_product_sales.update_layout(
+#---- Display the DataFrame on Streamlit app ----
+st.subheader('Categories Data')
+st.dataframe(df)
+
+st.subheader('Market Capitalization by Categories')
+st.write('This is a bar_chart.')
+fig_mc = px.histogram(y=df_selection["market_cap"], x=df_selection["name"], title="Daily Market Cap by Categories", template="plotly_white", color_discrete_sequence=px.colors.diverging.Spectral).update_xaxes(categoryorder="total descending")
+fig_mc.update_traces(ybins_size=1)
+fig_mc.update_layout(
+    autosize=False,
+    width=1100,
+    height=500,
     plot_bgcolor="rgba(0,0,0,0)",
+    xaxis_title="Market Capitalization in USD",
+    yaxis_title="Crypto Categories",
+    legend_title="Legend Title",
+    xaxis=(dict(showgrid=False))
+)
+st.plotly_chart(fig_mc)
+
+fig_vl = px.histogram(y=df_selection["volume_24h"], x=df_selection["name"], title="Daily Volume by Categories", template="plotly_white", color_discrete_sequence=px.colors.diverging.Spectral).update_xaxes(categoryorder="total descending")
+fig_vl.update_traces(ybins_size=1) # can add text=round(df_selection["volume_24h"], 1) if needed
+fig_vl.update_layout(
+    autosize=False,
+    width=1100,
+    height=500,
+    plot_bgcolor="rgba(0,0,0,0)",
+    xaxis_title="Crypto Categories",
+    yaxis_title="Volume in USD",
+    legend_title="Legend Title",
     xaxis=(dict(showgrid=False))
 )
 
-# SALES BY HOUR [BAR CHART]
-volume_chart = df_selection.groupby(by=["name"]).sum()[["volume_24h"]]
-fig_hourly_sales = px.bar(
-    volume_chart,
-    x=volume_chart.index,
-    y="name",
-    title="<b>Volume by Category</b>",
-    color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
-    template="plotly_white",
-)
-fig_hourly_sales.update_layout(
-    xaxis=dict(tickmode="linear"),
-    plot_bgcolor="rgba(0,0,0,0)",
-    yaxis=(dict(showgrid=False)),
-)
+st.plotly_chart(fig_vl)
 
-
+""""" To put the dashboard side by side : 
 left_column, right_column = st.columns(2)
-left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
-right_column.plotly_chart(fig_product_sales, use_container_width=True)
+left_column.plotly_chart(fig_mc, use_container_width=True) 
+right_column.plotly_chart(fig_vl, use_container_width=True) 
 
 
-# ---- HIDE STREAMLIT STYLE ----
+"""
+#The Line plot
+
+"""
+df = pandas.DataFrame(data=d)
+print(df)
+
+country_set = set(df['Country'])
+
+plt.figure()
+for country in country_set:
+     selected_data = df.loc[df['Country'] == country]
+     plt.plot(selected_data['Month'], selected_data['GDPA'], label=country)
+     
+plt.legend()
+plt.show()
+
+fig = px.line(
+        df, #Data Frame
+        x = "X_axis", #Columns from the data frame
+        y = "Y_axis",
+        title = "Line frame"
+    )
+fig.update_traces(line_color = "maroon")
+st.plotly_chart(fig)
+
++ Add the list of coins related to each categories
+"""""
+
+
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -109,8 +137,6 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-
-# In[ ]:
 
 
 
