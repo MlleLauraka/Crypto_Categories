c87c46121149749e9377909bea352da0209057bc
MlleLauraka <77595617+MlleLauraka@users.noreply.github.com>
2022-03-23 02:05:16 -0400

    Update and rename crypto_categories_streamlit.py to app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data, model # import your app modules here
+from apps import Crypto_Category, Model_Bank_Defaulters, SQClassProject,PycharmProjectsTableauCrypto_app # importing the app modules
 
 app = MultiApp()
 
@@ -9,7 +9,7 @@ st.markdown("""
 This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
 """)
 
-# Add all your application here
+# Adding all the applications
 app.add_app("Project 1 - Cryptocurrency Category Analysis", Crypto_Category.app)
 app.add_app("Project 2 - Banking Industry Churn Prediction", Model_Bank_Defaulters.app)
 app.add_app("Project 3 - Company Decision Support System", SQClassProject.app)
