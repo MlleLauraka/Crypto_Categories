6b6f6112e03aaff39c1345eec8c86150d8dcb06f
MlleLauraka <77595617+MlleLauraka@users.noreply.github.com>
2022-03-24 18:49:14 -0400

    Update SQClassProject.py

--- apps/SQClassProject.py
+++ apps/SQClassProject.py
@@ -7,7 +7,38 @@ from PIL import Image
 
 def app():
     st.title('SQL Company Metrics Analysis: Attrition Prediction')
+    
+    
+    # streamlit_app.py
 
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return connection.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from mytable;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
+    
+    mydb = conn
+    query = "Select * from LGBRAND;"
+    result_dataFrame = pd.read_sql(query, mydb)
+    mydb.close()  # close the connection
+    
+"""    
     # About
     expander_bar = st.expander("About")
     expander_bar.markdown("""
@@ -309,4 +340,4 @@ def app():
     """
     result_dataFrameQ4b= pd.read_sql(query, mydb)
     mydb.close()  # close the connection
-
+ """
