fea055278438d546b61c22b94291c3cacfa7ff3c
MlleLauraka <77595617+MlleLauraka@users.noreply.github.com>
2022-04-15 01:45:38 -0400

    Update Project_VF.py

--- Project_VF.py
+++ Project_VF.py
@@ -919,44 +919,51 @@ elif choose =="Crypto Exchanges Analysis with Tableau":
                             *The Histogram* sorts the list of Exchanges by Daily Volume and match with the Map Chart to show the exact list of Exchanges by Country.\n\n
                             *The TreeMap* shows the Exchanges by Trust Score. Trust Score is a metric delivered by Coin Gecko giving an Idea of the Trust you can have in those Exchanges.\n\n 
                             **Legend**: You can also research an Exchange by it's Id.
-
                             """)
 
         # the body of the page
 
-
-    def main():
-        html_temp = """<div class='tableauPlaceholder' id='viz1647894688149' style='position: relative'>
+    def main():   
+         html_temp = """<div class='tableauPlaceholder' id='viz1650000855821' style='position: relative'>
             <noscript>
             <a href='#'>
-            <img alt='Top 100 Crypto Exchanges Analysis (Daily) Have you ever asked yourself whose crypto exchange perform the best and from where they operate their magic?This Dashboard gives a daily analysis of the top 100 Crypto Exchanges listed on the CoinGecko Website.That is an interesting tool to anticipate the effect of countries regulation on the differents exchanges or, to get an idea of the best place to operate an Cryptoccurency Exchange.This tool can also be on an help for people looking for investing in Digital Assets since the Volume and Trust Score are indicators of Exchanges performance and investors can rely on: The Trust Score established by CoinGecko lead us to keep the difference between the CEX (Centralized Exchanges) and DEX (Decentralized Exchanges) in mind. Indeed, it appears that the majority of DEX exchanges have the lowest Trust Scores even though their daily Volume can be consequent (Please view the Uniswap Volume (v3)).
-            ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;To&#47;Top100CryptoExchangesAnalysisDaily&#47;Story1&#47;1_rss.png' style='border: none' />
+            <img alt='Top 100 crypto Exchanges by Traded VolumeHave you ever wondered which crypto-currency exchanges are the most successful and from where they work their magic from?This dashboard presents an analysis of the top 100 crypto-currency exchanges listed on Coi ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;To&#47;Top100CryptoExchanges&#47;Story1&#47;1_rss.png' style='border: none' />
             </a>
-            </noscript><object class='tableauViz'  style='display:none;'>
+            </noscript>
+            <object class='tableauViz'  style='display:none;'>
             <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
-            <param name='embed_code_version' value='3' /> <param name='site_root' value='' />
-            <param name='name' value='Top100CryptoExchangesAnalysisDaily&#47;Story1' />
-            <param name='tabs' value='no' /><param name='toolbar' value='yes' />
-            <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;To&#47;Top100CryptoExchangesAnalysisDaily&#47;Story1&#47;1.png' />
-            <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' />
-            <param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' />
-            <param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                
-            <script type='text/javascript'>                    
-            var divElement = document.getElementById('viz1647894688149');                    
+            <param name='embed_code_version' value='3' /> 
+            <param name='site_root' value='' />
+            <param name='name' value='Top100CryptoExchanges&#47;Story1' />
+            <param name='tabs' value='no' />
+            <param name='toolbar' value='yes' />
+            <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;To&#47;Top100CryptoExchanges&#47;Story1&#47;1.png' /> 
+            <param name='animate_transition' value='yes' />
+            <param name='display_static_image' value='yes' />
+            <param name='display_spinner' value='yes' />
+            <param name='display_overlay' value='yes' />
+            <param name='display_count' value='yes' />
+            <param name='language' value='en-US' />
+            <param name='filter' value='publish=yes' />
+            </object>
+            </div>                
+            <script type='text/javascript'>                   
+        
+            var divElement = document.getElementById('viz1650000855821');                    
             var vizElement = divElement.getElementsByTagName('object')[0];                    
             vizElement.style.width='1016px';vizElement.style.height='991px';                    
             var scriptElement = document.createElement('script');                    
             scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
             vizElement.parentNode.insertBefore(scriptElement, vizElement);                
             </script>"""
-        components.html(html_temp, width=1130, height=1100)
+            components.html(html_temp, width=1130, height=1100)
 
 
     if __name__ == "__main__":
         main()
 
         st.markdown(
-            f'Link to the dashboard on Tableau Public [here](https://public.tableau.com/app/profile/laura5733/viz/Top100CryptoExchangesAnalysisDaily/Story1#1)')
+            f'Link to the dashboard on Tableau Public [here](https://public.tableau.com/views/Top100CryptoExchanges/Story1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link)')
         st.markdown(
             f"**Data source and information about data collect can be found on [Coin Gecko - API](https://www.coingecko.com/en/api)**")
         max_width_str = f"max-width: 1030px;"
@@ -969,6 +976,7 @@ elif choose =="Crypto Exchanges Analysis with Tableau":
     def load_page():
         sidebar_info()
         main()
+
 
 
 elif choose =="Business Performance Analysis with SQL":
